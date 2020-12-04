cbd10007315ec3e2cbd6b112fb9b94bf987fe5af
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-04 23:38:14 +0100

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,14 +1,14 @@
-import numpy as np
-import pandas as pd
+import json
 
-def handler(event, context):
+
+def lambda_handler(event, context):
+    return response('Hello World!', 200)
+
+
+def response(message, status_code):
     return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': 'Hello from Lambda!'
-        ,
-        "isBase64Encoded": False
+        'isBase64Encoded': False,
+        'statusCode': status_code,
+        'body': json.dumps(message),
+        'headers': {'Content-Type': 'application/json'}
     }
