62a98aea07af3d37e86b3b96d4fd36a709bc4f49
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-12 00:31:49 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -3,4 +3,12 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CUSTOMERS')
 def lambda_handler2(event, context):
     table.put_item(Item=event)
-    return {"code":200,"message":"Customer Added Successfully"}
+    response = {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true'
+        },
+        "body": json.dumps(item)
+    }
+    return response
