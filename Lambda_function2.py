1bbddb4806eb853bcac8fe4ebbb11a386523cd24
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 00:32:01 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -1,12 +1,18 @@
 import boto3
-
 def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
-    PARTTION_KEY = 'CUSTOMER_ID'
     table.put_item(
-    Item={
-        PARTITION_KEY: {'S': 'my-part-key'}
-    }
+        Item ={
+              "CUSTOMER_ID":'1234',
+              "CUST_NAME":'CARTESOFT',
+              "LOCATION":'PLATEN'
+            
+            
+        }
+                  
+                  
 )
+    
     return {"Code": 200, "message":"Customer Added Successfully"}
+
