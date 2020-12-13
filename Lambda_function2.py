1a1f0d43296382c886027b2e444dc48329db5fef
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-13 23:49:36 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -1,8 +1,2 @@
-import boto3
-import json
 def lambda_handler2(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('CUSTOMERS')
-    response = table.put_item(Item = event)
-    print(json.dumps(response))
-    print("Row-" + str(index) + " written to DynamoDB successfully")
+    return str(event)
