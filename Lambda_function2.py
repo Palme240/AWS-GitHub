d342574a009473981437da419f159987fdba8889
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 11:35:32 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -4,10 +4,13 @@ import json
 def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
-    it = json.loads(event['records'][0]['data'])
-    table.put_item(Item = it )
+    table.put_item(Item = event )
     
-    return {"Code": 200, "message":"Customer Added Successfully"}
+    return {
+            'statusCode': 200,
+            "headers": {"Content-Type": "application/json"},
+            'body': json.loads(json_data)
+            }
 
     
 
