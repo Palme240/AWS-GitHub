f0cc25559f90c66f0e856318d32fe9b80be0c633
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 11:20:36 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+from boto3.dynamodb.conditions import Key, Attr
+
 def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
