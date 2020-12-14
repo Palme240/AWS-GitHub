1f0d6d82253d6bac7f621ccda74139486652b5df
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 14:14:31 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -7,4 +7,4 @@ def lambda_handler2(event, context):
     body = json.loads(event['body'])
     table.put_item(Item = body)
     
-    return {"statusCode": 200, "body": "results"}
+    return {"statusCode": 200, "body": "Customer Added Successfully"}
