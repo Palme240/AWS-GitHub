d11048f140c8334760168602e8e5185ab9bdbce1
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 12:25:26 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -1,17 +1,27 @@
 import boto3
+
 def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
-    table.put_item(
-        Item = event {
-              "CUSTOMER_ID":'1235',
-              "CUST_NAME":'PROCOM',
-              "LOCATION":'PLATEN'
-            
-            
-        }
-                  
-                  
-)
-    
-    return {"Code": 200, "message":"Customer Added Successfully"}
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    operations = {
+        'POST': lambda dynamo, x: table.put_item(**x)
+    }
+
+    operation = event['httpMethod']
+    if operation in operations:
+        payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
+        dynamo = boto3.resource('dynamodb').Table(payload['TableName'])
+        return respond(None, operations[operation](dynamo, payload))
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation))) 
