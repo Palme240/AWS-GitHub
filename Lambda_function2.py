c92ddc33a4a081b58be76c0904d1fb5bdba8ce94
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 10:47:58 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -4,9 +4,17 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CUSTOMERS')
    
 def lambda_handler2(event, context):
-   print(json.dumps(event))
    response = table.put_item(Item = event)
-   print(json.dumps(response))
+   if not event.get('body'):
+        return respond({"message": "PUT request requires parameters in the body"})
+
+    try:
+        body = json.loads(event['body'])
+    except:
+        return respond({
+            "message": "PUT request requires JSON parameters in the body",
+            "body": event['body']
+        })
    print("Row-" + str(index) + " written to DynamoDB successfully")
     
 
