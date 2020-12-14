b10ec9baf3b83c40b7c2498c3cdaa19955ee7e32
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 12:53:41 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -3,6 +3,6 @@ def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
     print (event)
-    table.put_item(Item = event.body)
+    table.put_item(Item = event['body'])
     
     return {"Code": 200, "message":"Customer Added Successfully"}
