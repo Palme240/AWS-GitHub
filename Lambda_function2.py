fddc693dd9536bbad11188a99712abc696073e70
Palme240 <69111687+Palme240@users.noreply.github.com>
2020-12-14 14:21:30 +0100

    Update Lambda_function2.py

--- Lambda_function2.py
+++ Lambda_function2.py
@@ -3,7 +3,6 @@ import json
 def lambda_handler2(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('CUSTOMERS')
-    print (event)
     body = json.loads(event['body'])
     table.put_item(Item = body)
     
